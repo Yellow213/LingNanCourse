f7208303bd602d88c3ab5d6ec1a35f4800b00148
CSS <CSS@837-wuby.CHOWSANGSANG>
2018-12-10 14:42:05 +0800

    L2018121001 添加jar包

---
+++ src/com/css/lingnan/course/test/HelloWoldTest.java
@@ -0,0 +1,17 @@
+package com.css.lingnan.course.test;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+public class HelloWoldTest {
+	protected Log logger = LogFactory.getLog(this.getClass().getName());
+	@Test
+	public void test() {
+		
+		logger.info("测试类......");//提取access_token节点数据
+	}
+
+}
